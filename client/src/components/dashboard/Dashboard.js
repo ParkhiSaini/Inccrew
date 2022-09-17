4a55767f0bc839f2a8b6eb27e0c91d2b0433c1e3
Parkhi Saini <parkhi.saini2401@gmail.com>
2022-09-17 13:18:07 +0530

    updated dashboard

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -2,71 +2,48 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { logoutUser } from "../../actions/authActions";
+
+import "./dashboard.css";
+
 class Dashboard extends Component {
   onLogoutClick = e => {
     e.preventDefault();
     this.props.logoutUser();
   };
-render() {
-   
-  const { name, email } = this.props.auth.user;
-return (
-  <header>
- 
-  <ul id="dropdown1" class="dropdown-content">
-    <li><a href="#!">Logout</a></li>
-  </ul>
-  <div class="navbar-fixed">
-    <nav class="red darken-1">
-      <div class="container">
-        <div class="nav-wrapper">
-          <a href="#!" class="brand-logo">Inncrew</a>
-          
-          <a href="#" data-target="nav-mobile" class="sidenav-trigger"><i class="material-icons">menu</i></a>
-          <ul class="right">
-          
-            <li><a class="dropdown-trigger" href="#!" data-target="dropdown1">Hello {email}<i class="material-icons right">arrow_drop_down</i></a></li>
-          </ul>
-        </div>
-      </div>  
-    </nav>
-  </div>
 
-  <ul id="nav-mobile" class="sidenav sidenav-fixed">
-      <li class="bold">
-        <a href="#" class="waves-effect waves-teal">Dashboard</a>
-      </li>
-      <li class="bold">
-        <a href="#" class="waves-effect waves-teal">My Events</a>
-      </li>
-      <li class="bold">
-        <a href="#" class="waves-effect waves-teal">Joined Events</a>
-      </li>
-      <li class="bold">
-        <a href="#" class="waves-effect waves-teal">My Schedule</a>
-      </li>
-      <li class="bold">
-        <a class="waves-effect waves-teal modal-trigger" href="#calendar">Calendar</a>
-      </li>
-      <li class="bold">
-        <a class="waves-effect waves-teal modal-trigger" href="#event-today">Events Today</a>
-      </li>
-      <div class="logo"><h3>Logo</h3></div>
-    </ul>
-  
-  
-  
-</header>
+  render() {
+    const { name,email} = this.props.auth.user;
+    
+    
+
+    return (
+      <header>
+ 
+ <nav>
+    <div class="nav-wrapper">
+     
+      <ul id="nav-mobile" class="#448aff blue accent-2right hide-on-med-and-down">
+        <li onClick={this.onLogoutClick}>Sign Out</li>
+        <li><a href="#">{name}</a></li>
+        <li><a href="#">{email}</a></li>
+        
+      </ul>
+    </div>
+  </nav>
+  </header>
     );
   }
 }
+
 Dashboard.propTypes = {
   logoutUser: PropTypes.func.isRequired,
   auth: PropTypes.object.isRequired
 };
+
 const mapStateToProps = state => ({
   auth: state.auth
 });
+
 export default connect(
   mapStateToProps,
   { logoutUser }
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,25 +1,16 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+
 class Navbar extends Component {
   render() {
     return (
-      
-        <nav className="z-depth-0">
-          <div className="nav-wrapper white">
-            <Link
-              to="/"
-              style={{
-                fontFamily: "monospace"
-              }}
-              className="col s5 brand-logo center black-text"
-            >                                                                                                                                                                                                                     
-             
-              Inncrew
-            </Link>
-          </div>
-        </nav>
-    
+      <div className="head">
+       
+          
+     
+      </div>
     );
   }
 }
+
 export default Navbar;
